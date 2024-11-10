517a1c176ffba4f8d8ce79f892389c1c0e4ebe16
LioV21 <villanuevalionel52104@gmail.com>
2024-11-10 13:14:57 -0800

    js script examples completed

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 // containing all the numbers from start up to (and including) end.
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+function range(start, end) {
+    let array = [];
+    for (let i = start; i <= end; i++) {
+        array.push(i);
+    }
+    return array;
+}
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
+function sum(array) {
+    let total = 0;
+    for (let number of array) {
+        total += number;
+    }
+    return total;
+}
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
@@ -17,7 +30,31 @@
 // method.
 // Example output for reverseArray:
 // console.log(reverseArray(["A", "B", "C"])); // outputs ["C", "B", "A"];
+function reverseArray(array) {
+    let reversed = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
+    return reversed;
+}
 // Example output for reverseArrayInPlace:
 // let arrayValue = [1, 2, 3, 4, 5];
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        let temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+}
+
+
+// Function Examples
+console.log(range(1, 10)); 
+console.log(sum(range(1, 10))); 
+console.log(reverseArray(["A", "B", "C"])); 
+
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue); 
